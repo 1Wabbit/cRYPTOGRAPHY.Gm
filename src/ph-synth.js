6bb6fda4b19413320ac85cdada046e11d899e5c9
1Wabbit <107828986+1Wabbit@users.noreply.github.com>
2020-06-21 08:48:23 +0000

    Fix format string

--- src/ph-synth.js
+++ src/ph-synth.js
@@ -16,3 +16,7 @@ fluid.defaults("floe.scienceLab.phSynth", {
                 // Alan, modulate this parameter, "distortion.amount",
                 // so that it increases at the poles.
                 // So it should be about 5.0 (or a little higher?)
+                // when the pH scale is at 0 or at 14,
+                // and it should be at 1.0 when we're at the midpoint of 7.
+                amount: 1.0,
+                mul: 0.5,
