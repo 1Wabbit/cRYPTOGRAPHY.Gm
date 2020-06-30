a578eef1544e20e941160fca8f5c9cec4b75dd9d
1Wabbit <107828986+1Wabbit@users.noreply.github.com>
2020-06-30 16:33:23 +0000

    Services squares

--- src/sonification.js
+++ src/sonification.js
@@ -3,3 +3,11 @@
  * A base component for sonification demos.
  */
 fluid.defaults("floe.scienceLab.sonification", {
+    gradeNames: "fluid.component",
+
+    distributeOptions: [
+        {
+            record: {
+                enviro: "{sonification}.enviro"
+            },
+            target: "{that flock.node}.options.components"
