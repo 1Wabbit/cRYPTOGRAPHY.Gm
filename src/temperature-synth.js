12837c96751a40d086c5ebaba498cc6518f99e5c
1Wabbit <107828986+1Wabbit@users.noreply.github.com>
2020-08-16 18:26:25 +0000

    Further development

--- src/temperature-synth.js
+++ src/temperature-synth.js
@@ -5,4 +5,4 @@ fluid.defaults("floe.scienceLab.temperatureBand", {
 
     components: {
         freqSynth: {
-            type: "floe.scienceLab.temperatureF
+            type: "floe.scienceLab.temperatureFre
