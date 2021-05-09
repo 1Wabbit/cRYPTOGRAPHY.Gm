df9aca3e059beec1e5fe76d0c493a0a4a6946418
1Wabbit <107828986+1Wabbit@users.noreply.github.com>
2021-05-09 18:33:36 +0000

    2.9.1

--- src/temperature-synth.js
+++ src/temperature-synth.js
@@ -39,4 +39,4 @@ fluid.defaults("floe.scienceLab.temperatureFreqSynth", {
                 id: "frequency",
                 ugen: "flock.ugen.value",
                 rate: "control",
-                // Alan, scale this value, "frequency.value", logarithmically between 120
+                // Alan, scale this value, "frequency.value", logarithmically between 120 an
