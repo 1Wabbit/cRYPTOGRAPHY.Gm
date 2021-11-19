0d2c7beadc87ec3d247127051cb3db6b375d64b4
1Wabbit <107828986+1Wabbit@users.noreply.github.com>
2021-11-19 17:44:21 +0000

    Added buttons

--- src/temperature-synth.js
+++ src/temperature-synth.js
@@ -61,4 +61,4 @@ fluid.defaults("floe.scienceLab.temperatureMixSynth", {
                 ugen: "flock.ugen.value",
                 rate: "control",
 
-                // Alan, scale this value, "mix.value", 
+                // Alan, scale this value, "mix.value", betwe
