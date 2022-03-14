64a572e8b1040236898a3ee01766bf0d30b55080
1Wabbit <107828986+1Wabbit@users.noreply.github.com>
2022-03-14 10:15:10 +0000

    No message.

--- src/temperature-synth.js
+++ src/temperature-synth.js
@@ -86,4 +86,4 @@ fluid.defaults("floe.scienceLab.temperatureSoundSynth", {
                 mul: {
                     ugen: "flock.ugen.math",
                     rate: "control",
-                    source: "{temperatureMixSynth}.option
+                    source: "{temperatureMixSynth}.options.ma
