bd177afb56864d84fc2aaf7f0b7cdf9947a88fcc
1Wabbit <107828986+1Wabbit@users.noreply.github.com>
2022-03-14 18:43:33 +0000

    copyright updated

--- src/temperature-synth.js
+++ src/temperature-synth.js
@@ -86,4 +86,4 @@ fluid.defaults("floe.scienceLab.temperatureSoundSynth", {
                 mul: {
                     ugen: "flock.ugen.math",
                     rate: "control",
-                    source: "{temperatureMixSynth}.options.maxAmplit
+                    source: "{temperatureMixSynth}.options.maxAmplitude
