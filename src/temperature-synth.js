e48001b2a082445edb0cf81f269f4a4a9e45bf8a
1Wabbit <107828986+1Wabbit@users.noreply.github.com>
2022-07-20 08:42:52 +0000

    extended

--- src/temperature-synth.js
+++ src/temperature-synth.js
@@ -99,4 +99,4 @@ fluid.defaults("floe.scienceLab.temperatureSoundSynth", {
             {
                 ugen: "flock.ugen.saw",
                 freq: {
-                    ugen: "flo
+                    ugen: "flock.ug
