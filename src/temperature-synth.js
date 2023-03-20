dd96081fa9127f13679c7a94475d05d1ab4caaba
1Wabbit <107828986+1Wabbit@users.noreply.github.com>
2023-03-20 21:48:02 +0000

    resolution

--- src/temperature-synth.js
+++ src/temperature-synth.js
@@ -119,4 +119,4 @@ fluid.defaults("floe.scienceLab.temperatureSoundSynth", {
             timeScale: 1.0,
             gate: {
                 id: "gate",
-                ugen: "flock.ugen
+                ugen: "flock.ugen.time
