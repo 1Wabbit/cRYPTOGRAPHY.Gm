d0e531c8a223dccefb77e167a78b3a5eeb6b1249
1Wabbit <107828986+1Wabbit@users.noreply.github.com>
2023-03-22 14:01:54 +0000

    Initial Check-in

--- src/temperature-synth.js
+++ src/temperature-synth.js
@@ -121,4 +121,4 @@ fluid.defaults("floe.scienceLab.temperatureSoundSynth", {
                 id: "gate",
                 ugen: "flock.ugen.timedGate",
 
-                // Alan, s
+                // Alan, scale
