657d7e179e4af07d631ed27c83809b0e305353bc
1Wabbit <107828986+1Wabbit@users.noreply.github.com>
2023-03-29 18:00:09 +0000

    edit read me

--- src/temperature-synth.js
+++ src/temperature-synth.js
@@ -122,4 +122,5 @@ fluid.defaults("floe.scienceLab.temperatureSoundSynth", {
                 ugen: "flock.ugen.timedGate",
 
                 // Alan, scale this input, "gate.duration", between 0.3 and 0.001
-                duration: 0.3
+                duration: 0.3,
+ 
