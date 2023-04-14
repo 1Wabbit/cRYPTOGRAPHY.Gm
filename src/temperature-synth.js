ac7876594fc65758133037e1ee47464a3c200782
1Wabbit <107828986+1Wabbit@users.noreply.github.com>
2023-04-14 19:33:03 +0000

    Upgrade `proof` to 5.0.x.

--- src/temperature-synth.js
+++ src/temperature-synth.js
@@ -126,4 +126,5 @@ fluid.defaults("floe.scienceLab.temperatureSoundSynth", {
                 trigger: {
                     ugen: "flock.ugen.impulse",
 
-                    // Alan, scale this input, "gate.trigger.freq", between 1 and 
+                    // Alan, scale this input, "gate.trigger.freq", between 1 and 8
+  
